0095b2239a3cbc43462259452a40296d11c6d769
alixdebeusscher <42241229+alixdebeusscher@users.noreply.github.com>
2018-12-18 12:07:36 +0100

    com

--- knn_regression.py
+++ knn_regression.py
@@ -1,126 +1,84 @@
 import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt 
 from sklearn import neighbors
-from sklearn.model_selection import learning_curve
-import matplotlib.pyplot as mp
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error 
 from math import sqrt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
-def score_function(model, X, Y):
-    'norm-2 criterion for optimization of models'
-    return np.sqrt(np.mean(((model.predict(X) - Y)) **2, axis=0)).sum()
+def normalize(X_train, X_test):
+    scaler = MinMaxScaler()
 
-def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
-                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 20), scoring=score_function):
-    """
-    Generate a simple plot of the test and training learning curve.
+    x_train_scaled = scaler.fit_transform(X_train)
+    x_train = pd.DataFrame(x_train_scaled)
+    
+    x_test_scaled = scaler.fit_transform(X_test)
+    x_test = pd.DataFrame(x_test_scaled)
+    
+    return x_train, x_test
 
-    Parameters
-    ----------
-    estimator : object type that implements the "fit" and "predict" methods
-        An object of that type which is cloned for each validation.
+def get_k(X_train,X_test,y_train,y_test,max_k):
+    rmse_val = [] #to store rmse values for different k
+    for K in range(1,max_k+1):
+        model = neighbors.KNeighborsRegressor(n_neighbors = K)
+        model.fit(x_train, y_train)  #fit the model
+        pred=model.predict(x_test) #make prediction on test set
+        error = sqrt(mean_squared_error(y_test,pred)) #calculate rmse
+        rmse_val.append(error) #store rmse values
+        #print('RMSE value for k= ' , K , 'is:', error)
+        
+    #PLOT K
+    curve = pd.DataFrame(rmse_val) #elbow curve 
+    curve.plot()
+    k = min(rmse_val)
+    p = rmse_val.index(min(rmse_val))
+    return k,p
 
-    title : string
-        Title for the chart.
+def linear():
+    regressor = LinearRegression()
+      
+    regressor.fit(X_train, y_train)  
+    #print('EIC', regressor.intercept_)
+    #print('NOC', len(regressor.coef_))
+    pred = regressor.predict(X_test)  
+    error = sqrt(mean_squared_error(y_test,pred)) #calculate rmse
+    print('error of linear is', error)
+    
+    k = 0
+    tab = [0, 0, 0, 0, 0, 0, 0, 0]
+    for each in col[:-1]:
+        for i in range(10000):
+            tab[k]+=regressor.coef_[k]/10000
+        #print("ECoef of %s is: %f", each, tab[k])        
+        k+=1
+    
+    y = tab
+    N = len(y)
+    x = range(N)
+    width = 1/1.5
+    plt.subplots(1)
+    plt.bar(x, y, width, color="green")
+    
+    
+  #  fig = plt.gcf()
 
-    X : array-like, shape (n_samples, n_features)
-        Training vector, where n_samples is the number of samples and
-        n_features is the number of features.
-
-    y : array-like, shape (n_samples) or (n_samples, n_features), optional
-        Target relative to X for classification or regression;
-        None for unsupervised learning.
-
-    ylim : tuple, shape (ymin, ymax), optional
-        Defines minimum and maximum yvalues plotted.
-
-    cv : int, cross-validation generator or an iterable, optional
-        Determines the cross-validation splitting strategy.
-        Possible inputs for cv are:
-          - None, to use the default 3-fold cross-validation,
-          - integer, to specify the number of folds.
-          - An object to be used as a cross-validation generator.
-          - An iterable yielding train/test splits.
-
-        For integer/None inputs, if ``y`` is binary or multiclass,
-        :class:`StratifiedKFold` used. If the estimator is not a classifier
-        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
-
-        Refer :ref:`User Guide <cross_validation>` for the various
-        cross-validators that can be used here.
-
-    n_jobs : integer, optional
-        Number of jobs to run in parallel (default 1).
-    """
-    plt = mp.figure()
-    mp.title(title)
-    if ylim is not None:
-        mp.ylim(*ylim)
-    mp.xlabel("Training examples")
-    mp.ylabel("Score")
-    train_sizes, train_scores, test_scores = learning_curve(
-        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes, scoring=scoring)
-    train_scores_mean = np.mean(train_scores, axis=1)
-    train_scores_std = np.std(train_scores, axis=1)
-    test_scores_mean = np.mean(test_scores, axis=1)
-    test_scores_std = np.std(test_scores, axis=1)
-    mp.grid()
-
-    mp.fill_between(train_sizes, train_scores_mean - train_scores_std,
-                     train_scores_mean + train_scores_std, alpha=0.1,
-                     color="r")
-    mp.fill_between(train_sizes, test_scores_mean - test_scores_std,
-                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
-    mp.plot(train_sizes, train_scores_mean, 'o-', color="r",
-             label="Training score")
-    mp.plot(train_sizes, test_scores_mean, 'o-', color="g",
-             label="Cross-validation score")
-
-    mp.legend(loc="best")
-    return plt
-
+    
 #Read test file
 X1=pd.read_csv("X1_t1.csv")
 
-#Split test file in learning set and test set
-X = X1.drop('ccs',axis=1).values
-y = X1['ccs'].values
+#column name
+col = list(X1)
 
+#Split test file in learning set and test set
+X = X1.drop(col[-1],axis=1).values
+y = X1[col[-1]].values
+
+#split data
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3, shuffle=True)
 
-#D'autres moyen de normaliser ?????????? je comprends pas trop cette section
-scaler = MinMaxScaler(feature_range=(0, 1))
-
-x_train_scaled = scaler.fit_transform(X_train)
-x_train = pd.DataFrame(x_train_scaled)
-
-x_test_scaled = scaler.fit_transform(X_test)
-x_test = pd.DataFrame(x_test_scaled)
-
-    
-rmse_val = [] #to store rmse values for different k
-for K in range(22):
-    K = K+1
-    model = neighbors.KNeighborsRegressor(n_neighbors = K)
-    model.fit(x_train, y_train)  #fit the model
-    pred=model.predict(x_test) #make prediction on test set
-    error = sqrt(mean_squared_error(y_test,pred)) #calculate rmse
-    rmse_val.append(error) #store rmse values
-    print('RMSE value for k= ' , K , 'is:', error)
-    
-
-plot_learning_curve(model, "title", X, y, ylim=None, cv=None, n_jobs=1, train_sizes=np.linspace(.1, 1.0, 20), scoring=score_function)
-#x axis pas ok
-curve = pd.DataFrame(rmse_val) #elbow curve 
-plt = curve.plot()
-
-k = min(rmse_val)
-print(k)
-#grpah commence à 0 mais c'est faux
-X2=pd.read_csv("X2.csv")
-pred = model.predict(X2) 
-#print(pred)
-#save pred to csv
-
+#normalize data
+x_train, x_test = normalize(X_train, X_test)
+rmse, k = get_k(X_train,X_test,y_train,y_test,30)
+print('Least rmse is:', rmse, 'with k:', k)
+linear()
--- outliers.py
+++ outliers.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 #Read test file
 X1=pd.read_csv("X1_t1.csv")
 
@@ -12,28 +13,33 @@ colom = ['cement','blast','fly','water','superplas','coarse_agg','fine_agg','age
 X1_df=pd.DataFrame(X)
 X1_df.columns = colom
 #print(X1_df.head())
+scaler = MinMaxScaler()
+#
+x_train_scaled = scaler.fit_transform(X)
+Xf = pd.DataFrame(x_train_scaled)
+Xf.columns=colom
 
 import seaborn as sns
 #a=sns.boxplot(x=y)
 sns.set(style="whitegrid")
-tips = X1
+tips = Xf
 #ax = sns.boxplot(x=tips["ccs"])
 #ax = sns.swarmplot(x=tips["ccs"], color=".25")
 print(len(y))
 for i in range(len(y)):
  #  print(y[i])
     if y[i]>80.:
-       print('row to deleted :', i)
-       print(X[i])
+       print('row to deleted :', i, '\n')
+       print(tips.iloc[i])
        print(y[i])
       
-ag = sns.boxplot(x=tips["ccs"])
-ag = sns.swarmplot(x=tips["ccs"], color=".25")
+#ag = sns.boxplot(x=y)
+#ag = sns.swarmplot(x=y, color=".25")
 
 #fig, ax =plt.subplots(3,3)
-#sns.boxplot(y=tips['ccs'], ax=ax[0][0])
-#sns.swarmplot(y=tips['ccs'], ax=ax[0][0], color=".25")
-##sns.boxplot(y=tips['cement'], ax=ax[0][1])
+#sns.boxplot(y=y, ax=ax[0][0])
+##sns.swarmplot(y=y, ax=ax[0][0], color=".25")
+#sns.boxplot(y=tips['cement'], ax=ax[0][1])
 ##sns.swarmplot(y=tips['cement'], ax=ax[0][1], color=".25")
 #sns.boxplot(y=tips['blast'], ax=ax[0][2])
 ##sns.swarmplot(y=tips['blast'], ax=ax[0][2], color=".25")
@@ -49,4 +55,35 @@ ag = sns.swarmplot(x=tips["ccs"], color=".25")
 ##sns.swarmplot(y=tips['fine_agg'], ax=ax[2][1], color=".25")
 #sns.boxplot(y=tips['age'], ax=ax[2][2])
 ##sns.swarmplot(y=tips['age'], ax=ax[2][2], color=".25")
-fig.show()
+#fig.show()
+
+#fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(9, 4))
+
+# rectangular box plot
+#bplot1 = sns.boxplot(x=y)  # fill with color
+#bplot1 = sns.swarmplot(x=y, color=".25")
+#bplot1.set_xlabel('Boxplot of "ccs", target')
+#bplot1.set_ylabel('ylabel')
+
+# notch shape box plot
+bplot2 = sns.boxplot(y=Xf)
+#                         patch_artist=True)   # fill with color
+
+
+# fill with colors
+#colors = ['pink', 'lightblue', 'lightgreen', 'pink', 'lightblue', 'lightgreen', 'pink', 'lightblue']
+#for patch, color in zip(bplot2, colors):
+#       patch.set_facecolor(color)
+
+# adding horizontal grid lines
+#for ax in axes:
+#    ax.yaxis.grid(True)
+#    ax.set_xticks([y+1 for y in range(len(y))], )
+#    ax.set_xlabel('xlabel')
+#    ax.set_ylabel('ylabel')
+
+# add x-tick labels
+#plt.setp(axes, xticks=[y+1 for y in range(len(Xf))],
+        # xticklabels=['x1', 'x2', 'x3', 'x4', 'b', 'n', 'n', 'b'])
+
+#plt.show()
